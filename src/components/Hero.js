8f31893ba55e6c65cfff23f3f141b32a6fdc1b76
gufran0 <gullu2681@gmail.com>
2021-07-02 14:16:25 +0530

    initialcommit

---
+++ src/components/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Hero() {
+    return (
+        <header className="col-10 offset-1 hero-header">
+            <h1>GK QUIZ</h1>
+            <p>Select the appropricate answer for the pictures Shown.</p>
+        </header>
+    )
+}
+
+export default Hero;
